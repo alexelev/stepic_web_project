1b5480c094c260dffbaa2a7b2c491a4ba85dc6af
Alex <livalex11@gmail.com>
2016-03-10 11:47:29 +0000

    step with gunicorn passed

--- etc/init.sh
+++ etc/init.sh
@@ -1,5 +1,6 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo /home/box/web/gunicorn -b 0.0.0.0:8080 hello:app
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
